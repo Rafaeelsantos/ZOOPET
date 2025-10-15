9d4fec70d7f5e787f63c0c7ce7c600b7a1be2923
Rafaeelsantos <rafaelsantosfrr@gmail.com>
2025-10-14 22:31:34 -0300

    Alteração de formulário e estrutura de página inicial

--- paginas/pagina_de_cadastro_e_login/conexoes/login.php
+++ paginas/pagina_de_cadastro_e_login/conexoes/login.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['usuario_id'] = $usuario['id'];
             $_SESSION['usuario_nome'] = $usuario['nome'];
 
-            header("Location: ../../../inicio.php");
+            header("Location: ../../../index.php");
             exit;
         } else {
             // Senha incorreta
