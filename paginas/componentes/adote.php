b6926063401b6bf92171f15c18a2f609bfc26025
Rafaeelsantos <rafaelsantosfrr@gmail.com>
2025-10-16 22:30:48 -0300

    Alteração de formulário e estrutura de página inicial

--- paginas/componentes/adote.php
+++ paginas/componentes/adote.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 
-// Se não houver usuário logado, redireciona para login
+
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: paginas/pagina_de_cadastro_e_login/realizar_login.php");
     exit;
 }
 
-// Aqui você pode usar o nome do usuário
 $nome_usuario = $_SESSION['usuario_nome'];
 ?>
 
